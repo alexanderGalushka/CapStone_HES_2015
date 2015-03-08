1161c08c1a97b28485f6dc9501b3f089670cc9fa
Gerson Rodrigues <gerson.rodrigues@servian.com.au>
2015-03-08 18:11:13 +1100

    refactoring code

--- Adam/src/main/java/edu/harvard/cscie99/adam/io/ColumnReader.java
+++
@@ -1,6 +0,0 @@
-package edu.harvard.cscie99.adam.io;
-
-public class ColumnReader implements PlateReader {
-
-	//TODO
-}
--- Adam/src/main/java/edu/harvard/cscie99/adam/io/PlateReader.java
+++
@@ -1,7 +0,0 @@
-package edu.harvard.cscie99.adam.io;
-
-public interface PlateReader {
-	
-	
-
-}
