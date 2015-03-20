070763e68ca5be38f06f07ba49e40e501bf9fc47
gerson.rodrigues <gerson.rodrigues@R5003694.ffx.jfh.com.au>
2015-03-21 08:00:29 +1100

    Created methods for collaboration

--- adam/src/main/java/edu/harvard/cscie99/adam/controller/CollaborationController.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/controller/CollaborationController.java
@@ -1,12 +1,59 @@
 package edu.harvard.cscie99.adam.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
+import edu.harvard.cscie99.adam.profile.User;
 
 @RestController
 @RequestMapping(value = "/")
 public class CollaborationController {
 
-	//TODO
+	@RequestMapping(value = "/users/list", method = RequestMethod.GET)
+	@ResponseBody
+	public List<User> listAllUsers(){
+		
+		//TODO
+		return null;
+	}
+	
+	@RequestMapping(value = "/share/{projectId}/user/{username}", method = RequestMethod.GET)
+	@ResponseBody
+	public boolean addCollaboratorToProject(
+			@PathVariable("projectId") int projectId,
+			@PathVariable("username") String username,
+			@RequestParam(value="user", required=true) String user) throws UnauthorizedOperationException{
+		
+		//TODO
+		return true;
+	}
+	
+	@RequestMapping(value = "/unshare/{projectId}/user/{username}", method = RequestMethod.POST)
+	@ResponseBody
+	public boolean removeCollaboratorToProject(
+			@PathVariable("projectId") int projectId,
+			@PathVariable("username") String username,
+			@RequestParam(value="user", required=true) String user) throws UnauthorizedOperationException{
+		
+		//TODO
+		return true;
+	}
+	
+	@RequestMapping(value = "/unshare/{projectId}/", method = RequestMethod.POST)
+	@ResponseBody
+	public boolean removeAllCollaborators(
+			@PathVariable("projectId") int projectId,
+			@RequestParam(value="user", required=true) String user) throws UnauthorizedOperationException{
+		
+		//TODO
+		return true;
+	}
 	
 }
