716e705e9a7585679f7b91f362c63cb32a129703
gerson.rodrigues <gerson.rodrigues@R5003694.ffx.jfh.com.au>
2015-03-21 08:00:49 +1100

    Add attributes to User model class

--- adam/src/main/java/edu/harvard/cscie99/adam/profile/User.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/profile/User.java
@@ -6,6 +6,9 @@ public class User {
 	
 	private String username;
 	private String password;
+	private String email;
+	private String securityQuestion;
+	private String securityAnswer;
 	private List<String> roles;
 	
 	public String getUsername() {
@@ -25,6 +28,24 @@ public class User {
 	}
 	public void setRoles(List<String> roles) {
 		this.roles = roles;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
 	}
 
 }
