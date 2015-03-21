fb7866ec0608cd3c39598d9aecb589953df72e33
Adam <Adam@10.0.2.15>
2015-03-21 01:46:06 -0700

    Add method getUserDetails

--- adam/src/main/java/edu/harvard/cscie99/adam/service/AuthenticationService.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/service/AuthenticationService.java
@@ -2,11 +2,18 @@ package edu.harvard.cscie99.adam.service;
 
 import org.springframework.stereotype.Component;
 
+import edu.harvard.cscie99.adam.profile.User;
+
 @Component
 public class AuthenticationService {
 
 	public boolean checkUserAccess(String user, Integer plateId, String service) {
 		// TODO implement
 		return true;
+	}
+	
+	public User getUserDetails(String username){
+		//TODO implement
+		return null;
 	}
 }
