328120e80d12ec2c33278a1cd6d32379498cb532
Adam <Adam@10.0.2.15>
2015-03-21 06:58:10 -0700

    - Implemented seach and retrieveResults method with fake data

--- adam/src/main/java/edu/harvard/cscie99/adam/service/ResultService.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/service/ResultService.java
@@ -6,10 +6,19 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
+import edu.harvard.cscie99.adam.model.Plate;
 import edu.harvard.cscie99.adam.model.PlateResult;
 import edu.harvard.cscie99.adam.model.Sample;
+import edu.harvard.cscie99.adam.model.Well;
 import edu.harvard.cscie99.adam.model.WellResult;
+import edu.harvard.cscie99.adam.profile.User;
 
 @Component
 public class ResultService {
@@ -50,6 +59,65 @@ public class ResultService {
 		}
 		plateResult.setWells(wellResults);
 		return plateResult;
+	}
+	
+	public List<PlateResult> search(int projectId, int resultId, int plateId, Date creationDate, String comment, User owner){
+		
+		//TODO: search from DB
+		
+		List<PlateResult> results = new ArrayList<PlateResult>();
+		for (int i = 0; i < 10; i++){
+			
+			Plate plate = new Plate();
+			plate.setId(i);
+			plate.setDescription("description"+i);
+			plate.setName("plate"+i);
+			
+			List<WellResult> wells = new ArrayList<>();
+			for (int j = 0; j < 10; j++){
+				WellResult wellResult = new WellResult();
+				wellResult.setCreationTime(new Date());
+				wellResult.setId(j);
+				wells.add(wellResult);
+			}
+			
+			PlateResult result = new PlateResult();
+			result.setCreationDate(new Date());
+			result.setId(i);
+			result.setPlate(plate);
+			result.setWells(wells);
+			
+			results.add(result);
+		}
+		return results;
+	}
+	
+	
+
+	public PlateResult retrieveResult(int projectId, int resultId){
+		
+		//TODO retrieve from DB
+		
+		Plate plate = new Plate();
+		plate.setId(1);
+		plate.setDescription("description");
+		plate.setName("plate");
+		
+		List<WellResult> wells = new ArrayList<>();
+		for (int j = 0; j < 10; j++){
+			WellResult wellResult = new WellResult();
+			wellResult.setCreationTime(new Date());
+			wellResult.setId(j);
+			wells.add(wellResult);
+		}
+		
+		PlateResult result = new PlateResult();
+		result.setCreationDate(new Date());
+		result.setId(1);
+		result.setPlate(plate);
+		result.setWells(wells);
+		
+		return result;
 	}
 
 }
