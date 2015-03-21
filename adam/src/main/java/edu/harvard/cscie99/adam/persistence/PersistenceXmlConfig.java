6c4b6cf72a2ac6239b4be47385f69d894b9cecfe
Adam <Adam@10.0.2.15>
2015-03-21 14:33:24 -0700

    Spring + JPA configuration classes

---
+++ adam/src/main/java/edu/harvard/cscie99/adam/persistence/PersistenceXmlConfig.java
@@ -0,0 +1,18 @@
+package edu.harvard.cscie99.adam.persistence;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "edu.harvard.cscie99.adam"})
+@ImportResource({ "classpath:persistence.xml" })
+public class PersistenceXmlConfig {
+
+    public PersistenceXmlConfig() {
+        super();
+    }
+
+}
