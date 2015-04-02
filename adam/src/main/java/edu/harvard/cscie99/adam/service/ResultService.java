641289e0b5d40a278fe2cccfa10f9f458a01d3d6
Adam <Adam@10.0.2.15>
2015-04-01 19:30:32 -0700

    Changed the model

--- adam/src/main/java/edu/harvard/cscie99/adam/service/ResultService.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/service/ResultService.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
 import edu.harvard.cscie99.adam.model.Plate;
 import edu.harvard.cscie99.adam.model.PlateResult;
-import edu.harvard.cscie99.adam.model.Sample;
+//import edu.harvard.cscie99.adam.model.Sample;
 import edu.harvard.cscie99.adam.model.Well;
 import edu.harvard.cscie99.adam.model.WellResult;
 import edu.harvard.cscie99.adam.profile.User;
@@ -31,39 +31,40 @@ public class ResultService {
 	public PlateResult getPlateResult(Integer plateNumber){
 		
 		//TODO: Read data from DB
-		PlateResult plateResult = new PlateResult();
-		
-		List<WellResult> wellResults = new ArrayList<WellResult>();
-		
-		for (int i = 0; i< 100; i++){
-			WellResult wellResult = new WellResult();
-			
-			List<String> labels = new ArrayList<String>();
-			List<Sample> samples = new ArrayList<Sample>();
-
-			Calendar cal = Calendar.getInstance(); 
-		    cal.setTime(new Date());
-		    
-		    labels.add("compound1");
-		    labels.add("compound2");
-		    labels.add("compound3");
-		    labels.add("compound4");
-
-			for (int j = 0; j < 4; j++){
-				Sample sample = new Sample();
-				//sample.setValue(Math.random());
-				//sample.setTime(cal.getTime());
-				
-				cal.add(Calendar.HOUR_OF_DAY, 1);
-				samples.add(sample);
-			}
-			
-			//wellResult.setSamples(samples);
-			
-			wellResults.add(wellResult);
-		}
-		plateResult.setWells(wellResults);
-		return plateResult;
+//		PlateResult plateResult = new PlateResult();
+//		
+//		List<WellResult> wellResults = new ArrayList<WellResult>();
+//		
+//		for (int i = 0; i< 100; i++){
+//			WellResult wellResult = new WellResult();
+//			
+//			List<String> labels = new ArrayList<String>();
+//			List<Sample> samples = new ArrayList<Sample>();
+//
+//			Calendar cal = Calendar.getInstance(); 
+//		    cal.setTime(new Date());
+//		    
+//		    labels.add("compound1");
+//		    labels.add("compound2");
+//		    labels.add("compound3");
+//		    labels.add("compound4");
+//
+//			for (int j = 0; j < 4; j++){
+//				Sample sample = new Sample();
+//				//sample.setValue(Math.random());
+//				//sample.setTime(cal.getTime());
+//				
+//				cal.add(Calendar.HOUR_OF_DAY, 1);
+//				samples.add(sample);
+//			}
+//			
+//			//wellResult.setSamples(samples);
+//			
+//			wellResults.add(wellResult);
+//		}
+//		plateResult.setWells(wellResults);
+//		return plateResult;
+		return null;
 	}
 	
 	public List<PlateResult> search(int projectId, int resultId, int plateId, Date creationDate, String comment, User owner){
