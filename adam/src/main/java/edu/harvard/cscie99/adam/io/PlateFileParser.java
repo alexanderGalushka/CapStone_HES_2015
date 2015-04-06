9922360f1b2587d344bfeaa81c6eb666439c295f
Gerson <Adam@Adam-PC>
2015-04-05 18:18:34 -0700

    Plate parsing bugfixes

--- adam/src/main/java/edu/harvard/cscie99/adam/io/PlateFileParser.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/io/PlateFileParser.java
@@ -2,8 +2,12 @@ package edu.harvard.cscie99.adam.io;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
 import org.springframework.stereotype.Component;
+
+
 
 
 //import edu.harvard.cscie99.adam.model.Compound;
@@ -24,6 +28,9 @@ public class PlateFileParser {
 		
 		Plate plate = new Plate();
 		
+		HashMap<String, Well> wellsMap = new HashMap<String, Well>();
+		HashSet<String> plateLabelNames = new HashSet<String>();
+		
         while (line != null) {
         	String[] fields = line.split(",");
         	
@@ -39,42 +46,51 @@ public class PlateFileParser {
         	
         	try{
         		
-        		int row = Integer.parseInt(fields[0]);
-        		int col = Integer.parseInt(fields[1]);
-        		String wellType = fields[2];
-        		String labelName = fields[3];
-        		String labelValue = fields[4];
-        		String compound = fields[5];
-        		double quantity = Double.parseDouble(fields[6]);
+        		int row = Integer.parseInt(fields[0].trim());
+        		int col = Integer.parseInt(fields[1].trim());
+        		String wellType = fields[2].trim();
+        		String labelName = fields[3].trim();
+        		String labelValue = fields[4].trim();
+        		String compound = fields[5].trim();
+        		double quantity = Double.parseDouble(fields[6].trim());
         		String unit = null;
         		
         		if (fields.length >= 8){
         			unit = fields[7];
         		}
         		
-        		Well well = new Well();
-        		if (Well.ControlType.NEGATIVE.equals(wellType) ){
-        			well.setControlType(Well.ControlType.NEGATIVE);
+        		Well well = null;
+        		if (wellsMap.containsKey(row + "-" + col)){
+        			well = wellsMap.get(row + "-" + col);
+        			
+        		}else{
+        			well = new Well();
+        			wellsMap.put(row+"-"+col, well);
+        			
+        			if (Well.ControlType.NEG.toString().equals(wellType) ){
+            			well.setControlType(Well.ControlType.NEG);
+            		}
+            		else if (Well.ControlType.POS.toString().equals(wellType)){
+            			well.setControlType(Well.ControlType.POS);
+            		}
+            		else if (Well.ControlType.COMP.toString().equals(wellType)){
+            			well.setControlType(Well.ControlType.COMP);
+            		}
+            		else if (Well.ControlType.EMPTY.toString().equals(wellType)){
+            			well.setControlType(Well.ControlType.EMPTY);
+            		}
+            		
+            		well.setRow(row);
+            		well.setCol(col);
         		}
-        		else if (Well.ControlType.POSITIVE.equals(wellType)){
-        			well.setControlType(Well.ControlType.POSITIVE);
-        		}
-        		else if (Well.ControlType.COMPOUND.equals(wellType)){
-        			well.setControlType(Well.ControlType.COMPOUND);
-        		}
-        		else if (Well.ControlType.EMPTY.equals(wellType)){
-        			well.setControlType(Well.ControlType.EMPTY);
-        		}
-        		
-        		well.setPlatePositionX(row);
-        		well.setPlatePositionY(col);
         		
         		WellLabel label = new WellLabel();
         		label.setName(labelName);
         		label.setValue(labelValue);
         		
         		well.getWellLabels().add(label);
-        		plate.getWells().add(well);
+//        		plate.getWells().add(well);
+        		plateLabelNames.add(labelName);
         		        		
         	}
         	catch (Exception ex){
@@ -82,6 +98,20 @@ public class PlateFileParser {
         	}
             
             line = lines.readLine();
+        }
+        
+        //add Wells to plate
+        for (String wellKey : wellsMap.keySet()){
+        	Well well = wellsMap.get(wellKey);
+        	plate.getWells().add(well);
+        }
+        
+        //add Label names to plate
+        for (String labelName : plateLabelNames){
+        	WellLabel label = new WellLabel();
+        	label.setName(labelName);
+        	
+        	plate.getWellLabels().add(label);
         }
 	    
 		return plate;
