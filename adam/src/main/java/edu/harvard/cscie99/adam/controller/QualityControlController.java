be0dfafd4ce63319d1f2a02d66312e858b2a43f5
Adam <Adam@10.0.2.15>
2015-04-11 04:14:32 -0700

    Fixing deploy issue

--- adam/src/main/java/edu/harvard/cscie99/adam/controller/QualityControlController.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/controller/QualityControlController.java
@@ -1,38 +1,38 @@
-package edu.harvard.cscie99.adam.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
-import edu.harvard.cscie99.adam.model.DataSet;
-import edu.harvard.cscie99.adam.service.AuthenticationService;
-import edu.harvard.cscie99.adam.service.ParserService;
-import edu.harvard.cscie99.adam.service.QualityControlService;
-
-public class QualityControlController 
-{
-
-	@Autowired
-	private QualityControlService qualityControlService;
-
-	@Autowired
-	private ParserService parserService;
-	
-	@Autowired
-	private AuthenticationService authService;
-	
-	
-	@RequestMapping(value = "/project/{project_id}", method = RequestMethod.GET)
-	@ResponseBody
-	public List<DataSet> getNormalizedData(
-			@PathVariable("project_id") int projectId) throws  UnauthorizedOperationException{
-		
-		return qualityControlService.getNormalizedData(projectId);
-	}
-	
-}
+//package edu.harvard.cscie99.adam.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
+//import edu.harvard.cscie99.adam.model.DataSet;
+//import edu.harvard.cscie99.adam.service.AuthenticationService;
+//import edu.harvard.cscie99.adam.service.ParserService;
+//import edu.harvard.cscie99.adam.service.QualityControlService;
+//
+//public class QualityControlController 
+//{
+//
+//	@Autowired
+//	private QualityControlService qualityControlService;
+//
+//	@Autowired
+//	private ParserService parserService;
+//	
+//	@Autowired
+//	private AuthenticationService authService;
+//	
+//	
+//	@RequestMapping(value = "/project/{project_id}", method = RequestMethod.GET)
+//	@ResponseBody
+//	public List<DataSet> getNormalizedData(
+//			@PathVariable("project_id") int projectId) throws  UnauthorizedOperationException{
+//		
+//		return qualityControlService.getNormalizedData(projectId);
+//	}
+//	
+//}
