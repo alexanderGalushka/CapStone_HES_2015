a36461b8ed6e864dca4a9bfc0875e3cebb6ac7b6
Alexander Galushka <alexgalushka@gmail.com>
2015-04-15 08:29:00 -0700

    QualityControlServiceTest in progress

---
+++ adam/src/test/java/edu/harvard/cscie99/adam/service/QualityControlServiceTest.java
@@ -0,0 +1,150 @@
+//package edu.harvard.cscie99.adam.service;
+//
+//import static org.junit.Assert.*;
+//
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Set;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.support.AnnotationConfigContextLoader;
+//
+//import edu.harvard.cscie99.adam.config.PersistenceConfig;
+//import edu.harvard.cscie99.adam.config.PersistenceXmlConfig;
+//import edu.harvard.cscie99.adam.model.Plate;
+//import edu.harvard.cscie99.adam.model.Project;
+//import edu.harvard.cscie99.adam.model.QCdataTimeWrapper;
+//import edu.harvard.cscie99.adam.model.Well;
+//import edu.harvard.cscie99.adam.model.WellLabel;
+//
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(loader = AnnotationConfigContextLoader.class,
+//        classes = { PersistenceConfig.class, PersistenceXmlConfig.class })
+//public class QualityControlServiceTest {
+//
+//	@Autowired
+//    private SessionFactory sessionFactory;
+//	
+//	@Autowired
+//    private ProjectService projectService;
+//	
+//	@Autowired
+//    private QualityControlService qualityControlService;
+//	
+//	@Test
+//	public void test()
+//	{
+//		Project moqupProject = createProjectData();
+//
+//		Project createdProject = projectService.createProject(moqupProject);
+//
+//		Map<Integer, List<QCdataTimeWrapper>> resultsMap = qualityControlService.qualifyData(createdProject.getId());
+//		
+//		Set<Integer> setOfKeys = resultsMap.keySet();
+//		
+//		Integer arrayOfKeys[] = new Integer[setOfKeys.size()];
+//
+//		int i = 0;
+//		for(Integer key : setOfKeys)
+//		{
+//			arrayOfKeys[i] = key;
+//			i++;
+//		}
+//        // test for correct plate ID
+//		assertEquals(Integer.valueOf(100001), arrayOfKeys[0]);
+//		
+//		List<QCdataTimeWrapper> listOfQCdataTimeWrappers = resultsMap.get(100001);
+//		
+//		// Need the values, need time, etc.
+//		
+//		boolean isDone = projectService.deleteProject(createdProject);
+//		
+//		// when done with a test - delete project, close session 
+//		fail("Not yet implemented");
+//	}
+//
+//	
+//	
+//	private Project createProjectData()
+//	{
+//		
+//		Project project = new Project();
+//		Plate plate = new Plate();
+//		
+//		plate.setId(100001);
+//		
+//		ArrayList<WellLabel> wellLabels = new ArrayList<WellLabel>();
+//		WellLabel wellLabel = new WellLabel();
+//		wellLabel.setName("compound type");
+//		wellLabels.add(wellLabel);
+//		
+//		wellLabel = new WellLabel();
+//		wellLabel.setName("cell type");
+//		wellLabels.add(wellLabel);
+//		
+//		wellLabel = new WellLabel();
+//		wellLabel.setName("protein bind %");
+//		wellLabels.add(wellLabel);
+//		
+//		plate.setWellLabels(wellLabels);
+//		
+//		for (int i = 1; i < 12; i++)
+//		{
+//			for (int j = 1; j < 12; j++)
+//			{
+//				Well well = new Well();
+//				well.setRow(i);
+//				well.setCol(j);
+//				
+//				if (i == 1 && j == 2)
+//				{
+//					well.setIfValid(false);
+//				}
+//				
+//				if(i%3 == 0)
+//				{
+//					well.setControlType(Well.ControlType.NEG);
+//				}
+//				else if(i%3 == 1)
+//				{
+//					well.setControlType(Well.ControlType.POS);
+//				}
+//				else
+//				{
+//					well.setControlType(Well.ControlType.EMPTY);
+//				}
+//				
+//				wellLabels = new ArrayList<WellLabel>();
+//				wellLabel = new WellLabel();
+//				wellLabel.setName("cell type");
+//				wellLabel.setValue("zebra"+i);
+//				wellLabels.add(wellLabel);
+//				
+//				wellLabel = new WellLabel();
+//				wellLabel.setName("compound type");
+//				wellLabel.setValue("H" + i +"Cl"+j);
+//				wellLabels.add(wellLabel);
+//				
+//				wellLabel = new WellLabel();
+//				wellLabel.setName("protein bind %");
+//				wellLabel.setValue(i*j+"%");
+//				wellLabels.add(wellLabel);
+//				
+//				well.setWellLabels(wellLabels);
+//				plate.getWells().add(well);
+//			}
+//		}
+//		return project;
+//		
+//	}
+//}
+//
