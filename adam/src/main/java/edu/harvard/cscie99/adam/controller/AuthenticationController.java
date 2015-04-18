710892238c5a9db23c31181b7de009eb7ae0df40
Gerson <Adam@10.0.2.15>
2015-04-17 20:34:40 -0700

    Login functionality

--- adam/src/main/java/edu/harvard/cscie99/adam/controller/AuthenticationController.java
+++ adam/src/main/java/edu/harvard/cscie99/adam/controller/AuthenticationController.java
@@ -1,5 +1,11 @@
 package edu.harvard.cscie99.adam.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.harvard.cscie99.adam.error.LoginFailedException;
 import edu.harvard.cscie99.adam.error.SessionTimeouException;
 import edu.harvard.cscie99.adam.error.UnauthorizedOperationException;
+import edu.harvard.cscie99.adam.interceptor.AuthInterceptor;
 import edu.harvard.cscie99.adam.model.Project;
 import edu.harvard.cscie99.adam.profile.User;
 import edu.harvard.cscie99.adam.service.AuthenticationService;
@@ -26,13 +34,61 @@ public class AuthenticationController {
 	@Autowired
 	private AuthenticationService authService;
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	@ResponseBody
-	public User addCollaboratorToProject(
-			@RequestParam(value="user", required=true) String user,
-			@RequestParam(value="password", required=true) String password) throws UnauthorizedOperationException{
+	@RequestMapping(value = "/do_login", method = RequestMethod.GET)
+	public @ResponseBody User login( @RequestParam("username") String username,
+									 @RequestParam("password") String password,
+									 HttpServletRequest request, 
+									 HttpServletResponse response) throws LoginFailedException
+	{
+		HttpSession session = request.getSession();// it will go and look after the web.xml properties and check for the time out setting.
+
+	    //if User is already logged and session not expired
 		
-		//TODO
-		return null;
+		// not sure what getAttribute would return if the attribute is not set - need to check!
+		
+		Object userObject = session.getAttribute(AuthenticationService.C_USER_SESSION);
+		User user = null;
+		if(userObject == null)
+		{
+			//authenticationService will lookup database and match user and password
+			user = authService.validateCredentials(username, password);
+	       
+			if (user != null)
+			{
+				session.setAttribute(AuthenticationService.C_USER_SESSION, user);
+			}
+			else
+			{
+				throw new LoginFailedException("bad credentials");
+			}			
+		}
+		else
+		{
+			user = (User) userObject;
+		}
+		
+		//clear password for security
+		user.setPassword(null);
+	
+		return user;
+
+	}
+	
+	@RequestMapping(value = "/do_logout", method = RequestMethod.GET)
+	public boolean logout(HttpServletRequest request, 
+									 HttpServletResponse response) throws LoginFailedException
+	{
+		HttpSession session = request.getSession();// it will go and look after the web.xml properties and check for the time out setting.
+		session.invalidate();
+		
+		try {
+			response.sendRedirect(AuthInterceptor.C_LOGIN_SCREEN);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		
+		return true;
+
 	}
 }
