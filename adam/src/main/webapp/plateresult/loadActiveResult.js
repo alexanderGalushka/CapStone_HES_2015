ab23db6d942e7a0ccd5d43811d14b95dbaecb662
ibugarinovic <ibugarinovic@yahoo.com>
2015-05-04 14:17:55 -0400

    Adding frontend code to Adam Git handling incorrect plateresult

--- adam/src/main/webapp/plateresult/loadActiveResult.js
+++ adam/src/main/webapp/plateresult/loadActiveResult.js
@@ -15,43 +15,50 @@
       var foundFirstValidWell = false;
 
       plateres = Qc.get({"id": plateid},function(){
-        activePlateResult.plateResult = plateres;
-        activePlateResult.plateResult.options = {
-          from: 1,
-          to: 5,
-          floor: true,
-          step: 1,
-          dimension: "",
-          vertical: false,
-          scale: [],
-          callback: function(value, elt) {
+        if(plateres === null || plateres.timeStamps === null || plateres.timeStamps.length == 0 ||
+          plateres.measurements === null || plateres.measurements.length == 0){
+          activePlateResult.plateResult = plateres;
+          activePlateResult.plateResult.options = {
+            from: 1,
+            to: 5,
+            floor: true,
+            step: 1,
+            dimension: "",
+            vertical: false,
+            scale: [],
+            callback: function(value, elt) {
+            }
+          };
+          activePlateResult.plateResult.options.to = plateres.timeStamps.length;
+          for (var i = 0; i < plateres.timeStamps.length; i++) {
+            activePlateResult.plateResult.options.scale.push(plateres.timeStamps[i].value);
           }
-        };
-        activePlateResult.plateResult.options.to = plateres.timeStamps.length;
-        for (var i = 0; i < plateres.timeStamps.length; i++) {
-          activePlateResult.plateResult.options.scale.push(plateres.timeStamps[i].value);
-        }
-        activePlateResult.plateResult.activeMeasurement = plateres.measurements[0];
+          activePlateResult.plateResult.activeMeasurement = plateres.measurements[0];
 
-        for (var i = 0; i < plateres.measurements[0].wells.length; i++) {
-          if(parseFloat(plateres.measurements[0].wells[i].value) != 777) {
-            if (!foundFirstValidWell) {
-              minValue = parseFloat(plateres.measurements[0].wells[i].value);
-              maxValue = parseFloat(plateres.measurements[0].wells[i].value);
-              foundFirstValidWell = true;
-            } else {
-
-              if (minValue > parseFloat(plateres.measurements[0].wells[i].value))
+          for (var i = 0; i < plateres.measurements[0].wells.length; i++) {
+            if(parseFloat(plateres.measurements[0].wells[i].value) != 777) {
+              if (!foundFirstValidWell) {
                 minValue = parseFloat(plateres.measurements[0].wells[i].value);
-              else if (maxValue < parseFloat(plateres.measurements[0].wells[i].value))
                 maxValue = parseFloat(plateres.measurements[0].wells[i].value);
+                foundFirstValidWell = true;
+              } else {
 
+                if (minValue > parseFloat(plateres.measurements[0].wells[i].value))
+                  minValue = parseFloat(plateres.measurements[0].wells[i].value);
+                else if (maxValue < parseFloat(plateres.measurements[0].wells[i].value))
+                  maxValue = parseFloat(plateres.measurements[0].wells[i].value);
+
+              }
             }
           }
+          activePlateResult.plateResult.valuerange = {"minvalue":minValue, "maxvalue":maxValue};
+          activePlateResult.plateResult.valueslider = 1;
+          activePlateResult.resultExists = true;}
+        else{
+          console.log("incorrect plateResult returned" + JSON.stringify(plateResult, null, 4));
+          activePlateResult.plateResult = null;
+          activePlateResult.resultExists = false;
         }
-        activePlateResult.plateResult.valuerange = {"minvalue":minValue, "maxvalue":maxValue};
-        activePlateResult.plateResult.valueslider = 1;
-        activePlateResult.resultExists = true;
       }, function(error) {
         console.log(JSON.stringify(error, null, 4));
         activePlateResult.plateResult = null;
