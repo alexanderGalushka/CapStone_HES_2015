a562193147d03c96257ce984dc1600a2f88ff9c5
ibugarinovic <ibugarinovic@yahoo.com>
2015-05-04 14:39:57 -0400

    Adding frontend code to Adam Git handling incorrect plateresult

--- adam/src/main/webapp/plateresult/loadActiveResult.js
+++ adam/src/main/webapp/plateresult/loadActiveResult.js
@@ -15,8 +15,8 @@
       var foundFirstValidWell = false;
 
       plateres = Qc.get({"id": plateid},function(){
-        if(plateres === null || plateres.timeStamps === null || plateres.timeStamps.length == 0 ||
-          plateres.measurements === null || plateres.measurements.length == 0){
+        if(plateres != null && plateres.timeStamps != null && plateres.timeStamps.length != 0 &&
+          plateres.measurements != null && plateres.measurements.length != 0){
           activePlateResult.plateResult = plateres;
           activePlateResult.plateResult.options = {
             from: 1,
@@ -55,7 +55,7 @@
           activePlateResult.plateResult.valueslider = 1;
           activePlateResult.resultExists = true;}
         else{
-          console.log("incorrect plateResult returned" + JSON.stringify(plateResult, null, 4));
+          console.log("incorrect plateResult returned" + JSON.stringify(plateres, null, 4));
           activePlateResult.plateResult = null;
           activePlateResult.resultExists = false;
         }
