f34fbf331f9ca53056a27c4cac36155d63b14c34
Cindy Liu <cindyliu@post.harvard.edu>
2015-05-07 16:06:57 -0400

    attempt #3 to use ui-bootstrap dropdown

--- adam/src/main/webapp/dataanalysis/dataanalysis.js
+++ adam/src/main/webapp/dataanalysis/dataanalysis.js
@@ -884,7 +884,7 @@
 
   var DUMMYDEFAULTDATASERIESTOPLOT = [7, 8, 9];
 
-  app.controller('DropdownCtrl', ['$scope', '$log', 'DAService', '$http', '$q',
+  app.controller('DropdownCtrl', ['$scope', '$log', 'DAService', '$http', '$q', 'ui.bootstrap',
     function($scope, $log, DAService, $http, $q) {
 
       dsPLACEHOLDER = 'Select data series';
